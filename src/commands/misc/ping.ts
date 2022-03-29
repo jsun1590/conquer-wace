e87c40e219dbbe4ce5c990ddf8742d1537210ecc
jsun1590 <40915904+jsun1590@users.noreply.github.com>
2022-03-29 20:51:13 +0800

    cleaning up code

--- src/commands/misc/ping.ts
+++ src/commands/misc/ping.ts
@@ -10,9 +10,7 @@ module.exports = {
     // little trolling (yix).
     if (interaction.member.id == "291151071806750720") {
       await interaction.reply(
-        `:ping_pong: Pong! Latency: ${Math.round(
-          interaction.client.ws.ping
-        )}0ms`
+        `:ping_pong: Pong! Latency: ${Math.round(interaction.client.ws.ping)}0ms`
       );
     } else {
       await interaction.reply(
