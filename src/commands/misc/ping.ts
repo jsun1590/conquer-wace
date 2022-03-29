e9b80853371aea565335893ac54661cdb2825037
Jack Sun <jsun1590@gmail.com>
2022-03-29 21:25:23 +0800

    prettified file

--- src/commands/misc/ping.ts
+++ src/commands/misc/ping.ts
@@ -1,5 +1,5 @@
 import { SlashCommandBuilder } from "@discordjs/builders";
-import { Interaction } from "../../interfaces"
+import { Interaction } from "../../interfaces";
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -7,10 +7,10 @@ module.exports = {
     .setDescription("Test the bot's latency."),
   async execute(interaction: Interaction) {
     console.log(`${interaction.member.displayName} pinged!`);
-    // little trolling (yix).
 
-    let isYix = interaction.member.id == "291151071806750720"
-    let ping = Math.round(interaction.client.ws.ping).toString()
+    // little trolling (yix).
+    let isYix = interaction.member.id === "291151071806750720";
+    let ping = Math.round(interaction.client.ws.ping).toString();
     await interaction.reply(
       `:ping_pong: Pong! Latency: ${isYix ? ping + "0" : ping}ms`
     );
